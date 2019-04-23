9a53df892f0db06f0cc25a10c37884613ed76edd
gaozhen <gaozhen3h@163.com>
2019-04-23 14:29:31 +0800

    添加jwt
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- pig-video-gateway-service/src/main/java/com/gz/pigvideo/controller/UserWebRest.java
+++ pig-video-gateway-service/src/main/java/com/gz/pigvideo/controller/UserWebRest.java
@@ -2,22 +2,28 @@ package com.gz.pigvideo.controller;
 
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.gz.pigvideo.common.JWTUtil;
 import com.gz.pigvideo.domain.User;
+import com.gz.pigvideo.service.UserService;
 
 @RestController
-@RequestMapping("/webRest/user/")
+@RequestMapping("/userWebRest/")
 public class UserWebRest {
 
 	private static final Logger log = LoggerFactory.getLogger(UserWebRest.class);
+	
+	@Autowired
+	private UserService userService;
 	
 	/**
 	 * 用户登录
@@ -30,30 +36,26 @@ public class UserWebRest {
 		JSONObject response=new JSONObject();
 		try {
 			JSONObject request=JSONObject.parseObject(reqstr);
-			Subject currentuser=SecurityUtils.getSubject();
-			if(currentuser.isAuthenticated()){
-				log.info("current is Authenticated!");
-			}else{
-				String userName=request.getString("username");
-				String password=request.getString("password");
-				UsernamePasswordToken token=new UsernamePasswordToken(userName, password);
-				token.setRememberMe(true);
-				currentuser.login(token);
-				currentuser.getSession().setTimeout(-1001);
-				log.info(userName+" is login!");
+			String username=request.getString("username");
+			String password=request.getString("password");
+			User user = userService.getUserByAccount(username);
+			if(user.getPassword().equals(password)) {
+				JSONObject userObj = new JSONObject();
+				userObj.put("account", user.getAccount());
+				userObj.put("name", user.getName());
+				userObj.put("id", user.getUserId());
+				userObj.put("role", user.getRole().getRoleDetail());
+				userObj.put("menu", user.getRole().getMenu());
+				response.put("data", userObj);
+				response.put("token", JWTUtil.sign(username, password));
+				response.put("code", 2);
+			}else {
+				response.put("msg","用户名或密码错误");
+				response.put("code", 5);
 			}
-			User user = (User) currentuser.getSession().getAttribute("user");
-			JSONObject userObj = new JSONObject();
-			userObj.put("account", user.getAccount());
-			userObj.put("name", user.getName());
-			userObj.put("id", user.getUserId());
-			userObj.put("role", user.getRole().getRoleDetail());
-			response.put("data", userObj);
-			response.put("code", 2);
 		}catch(Exception e){
-			e.printStackTrace();
 			log.error(e.getMessage());
-			response.put("msg","密码或用户名错误");
+			response.put("msg","没有注册");
 			response.put("code", 5);
 		}
 		return response;
@@ -76,4 +78,23 @@ public class UserWebRest {
 		return response.toJSONString();
 	}
 
+	
+    @GetMapping("article")
+    public JSONObject article() {
+    	JSONObject response = new JSONObject();
+    	try {
+    		Subject subject = SecurityUtils.getSubject();
+    		if (subject.isAuthenticated()) {
+                response.put("code", 200);
+                response.put("msg", "You are already logged in");
+            }else {
+            	 response.put("code", 200);
+                 response.put("msg", "You are guest");
+            }
+		} catch (Exception e) {
+			response.put("code", 200);
+            response.put("msg", e.getMessage());
+		}
+    	return response;
+    }
 }
--- pig-video-gateway-service/src/main/java/com/gz/pigvideo/domain/Role.java
+++ pig-video-gateway-service/src/main/java/com/gz/pigvideo/domain/Role.java
@@ -4,16 +4,20 @@ public class Role implements java.io.Serializable {
     private Long roleId;//角色id
     private String roleDetail;//角色详情
     private String roleName;//角色名
+    private String permission;//权限
     private Integer delFlag;//存在标识
+    private String menu;//菜单
     public Role() {
         super();
     }
-    public Role(Long roleId,String roleDetail,String roleName,Integer delFlag) {
+    public Role(Long roleId,String roleDetail,String roleName,String permission,Integer delFlag,String menu) {
         super();
         this.roleId = roleId;
         this.roleDetail = roleDetail;
         this.roleName = roleName;
+        this.permission = permission;
         this.delFlag = delFlag;
+        this.menu = menu;
     }
     public Long getRoleId() {
         return this.roleId;
@@ -39,12 +43,28 @@ public class Role implements java.io.Serializable {
         this.roleName = roleName;
     }
 
+    public String getPermission() {
+        return this.permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
     public Integer getDelFlag() {
         return this.delFlag;
     }
 
     public void setDelFlag(Integer delFlag) {
         this.delFlag = delFlag;
+    }
+
+    public String getMenu() {
+        return this.menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
     }
 
 }
