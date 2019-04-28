7b2f6356e75b6fc43718149bc1b4256069f8470a
gaozhen <gaozhen3h@163.com>
2019-04-28 10:21:09 +0800

    添加权限修改功能
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- pig-video-auth-service/src/main/java/com/gz/pigvideo/controller/UserWebRest.java
+++ pig-video-auth-service/src/main/java/com/gz/pigvideo/controller/UserWebRest.java
@@ -1,6 +1,8 @@
 package com.gz.pigvideo.controller;
 
 
+import java.util.Date;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.gz.pigvideo.common.JWTUtil;
+import com.gz.pigvideo.domain.Role;
 import com.gz.pigvideo.domain.User;
 import com.gz.pigvideo.service.UserService;
 
@@ -39,6 +43,12 @@ public class UserWebRest {
 			String password=request.getString("password");
 			User user = userService.getUserByAccount(username);
 			if(user.getPassword().equals(password)) {
+				//修改登录时间
+				User para = new User();
+				para.setUserId(user.getUserId());
+				para.setLastLoginDate(new Date());
+				userService.updateNonEmptyUserById(para);
+				//返回值
 				JSONObject userObj = new JSONObject();
 				userObj.put("account", user.getAccount());
 				userObj.put("name", user.getName());
@@ -84,6 +94,64 @@ public class UserWebRest {
 		}
 		return response.toJSONString();
 	}
-
 	
+	@RequestMapping("listAllRole")
+	public JSONObject listAllRole() {
+		log.info("listAllRole");
+		JSONObject returnData = new JSONObject();
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			if(currentUser.isPermitted("role:select")){
+				returnData.put("code", 2);
+				returnData.put("data", userService.listAllRole());
+			}else {
+				returnData.put("code",4);
+				returnData.put("msg","亲，权限不足哟!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			returnData.put("code",5);
+			returnData.put("msg",e.getMessage());
+		}
+		return returnData;
+	}
+
+	@RequestMapping("updateRoleById")
+	public JSONObject updateRoleById(@RequestBody String reqstr) {
+		log.info(reqstr);
+		JSONObject response=new JSONObject();
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			if(currentUser.isPermitted("role:udpate")) {
+				JSONObject request=JSONObject.parseObject(reqstr);
+				JSONArray arr = request.getJSONArray("permission");
+				StringBuffer permissionsb = new StringBuffer();
+				for (Object object : arr) {
+					permissionsb.append(object+",");
+				}
+				Role role = new Role();
+				role.setRoleId(request.getLong("roleId"));
+				role.setMenu(request.getString("menu"));
+				role.setPermission(permissionsb.toString());
+				int effect = userService.updateNonEmptyRoleById(role);
+				if(effect==1) {
+					response.put("code", 2);
+				}else {
+					response.put("code", 5);
+					response.put("msg","修改角色发生异常");
+				}
+			}else {
+				response.put("msg","亲，权限不足哟!");
+				response.put("code", 4);
+			}
+		}catch(Exception e){
+			log.error(e.getMessage());
+			response.put("code", 5);
+			response.put("msg",e.getMessage());
+		}
+		return response;		
+	}
+	
+		
 }
--- pig-video-auth-service/src/main/java/com/gz/pigvideo/service/UserService.java
+++ pig-video-auth-service/src/main/java/com/gz/pigvideo/service/UserService.java
@@ -1,8 +1,12 @@
 package com.gz.pigvideo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.gz.pigvideo.domain.Role;
 import com.gz.pigvideo.domain.User;
 import com.gz.pigvideo.repository.RoleDao;
@@ -28,5 +32,32 @@ public class UserService {
 		Role role = roleDao.selectRoleById(user.getRoleId());
 		user.setRole(role);
 		return user;
+	}
+	
+	public JSONArray listAllRole() {
+		JSONArray arr = new JSONArray();
+		List<Role> roles = roleDao.selectRole(null);
+		for (Role role : roles) {
+			JSONObject obj = new JSONObject();
+			obj.put("roleId", role.getRoleId());
+			obj.put("roleName", role.getRoleName());
+			obj.put("roleDetail", role.getRoleDetail());
+			if(role.getPermission()!=null) {
+				obj.put("permission", role.getPermission().split(","));
+			}else {
+				obj.put("permission", null);
+			}
+			obj.put("menu",role.getMenu());
+			arr.add(obj);
+		}
+		return arr;
+	}
+	
+	public int updateNonEmptyRoleById(Role role){
+		return roleDao.updateNonEmptyRoleById(role);
+	}
+	
+	public int updateNonEmptyUserById(User user){
+		return userDao.updateNonEmptyUserById(user);
 	}
 }
