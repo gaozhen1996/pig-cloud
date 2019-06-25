338be41cfe93fdd5aef78406238110b883fda91d
gaozhen <gaozhen3h@163.com>
2019-06-25 21:54:40 +0800

    删除定时任务
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- pig-video-worm-service/src/main/java/com/gz/pigvideo/service/AutoWormService.java
+++ pig-video-worm-service/src/main/java/com/gz/pigvideo/service/AutoWormService.java
@@ -1,14 +1,9 @@
 package com.gz.pigvideo.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.alibaba.fastjson.JSONArray;
 import com.gz.pigvideo.remote.IVideoService;
-import com.gz.pigvideo.util.WormUtil;
 
 @Component
 public class AutoWormService {
@@ -17,8 +12,6 @@ public class AutoWormService {
     private int num = 3;
     
     private String url = "https://kuaiyinshi.com/#search-form";
-    
-    private static final Logger log = LoggerFactory.getLogger(AutoWormService.class);
     
     @Autowired
     IVideoService videoService;
@@ -47,13 +40,4 @@ public class AutoWormService {
 		this.url = url;
 	}
 
-	@Scheduled(fixedDelay = 1000*60*10)
-    public void autoWormTask(){
-        if (autoFlag){
-        	log.info("---------------自动爬取开始-------------------");
-            JSONArray urls = WormUtil.getCountVideoURL(url,num);
-            log.info(urls.toJSONString());
-            videoService.saveVideoURL(urls.toJSONString());
-        }
-    }
 }
