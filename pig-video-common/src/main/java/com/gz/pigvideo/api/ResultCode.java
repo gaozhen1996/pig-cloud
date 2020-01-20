21d07a05a411fa1d60c70fbbe67883f823641876
gaozhen1996 <3168175817@qq.com>
2020-01-20 17:05:21 +0800

    适应老版本前台

--- pig-video-common/src/main/java/com/gz/pigvideo/api/ResultCode.java
+++ pig-video-common/src/main/java/com/gz/pigvideo/api/ResultCode.java
@@ -1,13 +1,17 @@
 package com.gz.pigvideo.api;
 
+/**
+ * 
+ * @author gaozhen
+ */
 public enum ResultCode {
-    SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限"),
-	USERNOTEXISTS(410,"用户不存在"),
-	PASSWORDERROR(411,"密码错误");
+    SUCCESS(2, "操作成功"),
+    FAILED(5, "操作失败"),
+    VALIDATE_FAILED(4, "参数检验失败"),
+    UNAUTHORIZED(4, "暂未登录或token已经过期"),
+    FORBIDDEN(4, "没有相关权限"),
+	USERNOTEXISTS(4,"用户不存在"),
+	PASSWORDERROR(4,"密码错误");
 	
     private int code;
     
