04e02ff0e0269d9357b8d94356e456f7644032b2
固守青城 <3168175817@qq.com>
2021-07-19 18:59:04 +0800

    修改spring-boot-admin-server版本

--- pig-cloud-monitor-service/src/main/java/com/gz/pigcloud/MonitorApplication.java
+++ pig-cloud-monitor-service/src/main/java/com/gz/pigcloud/MonitorApplication.java
@@ -3,9 +3,7 @@ package com.gz.pigcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import de.codecentric.boot.admin.config.EnableAdminServer;
-
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 
 
