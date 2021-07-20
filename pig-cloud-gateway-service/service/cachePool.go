e7ef76cb9ed86998be6c28b906977e48e3e65727
gaozhen <gaozhen3h@163.com>
2021-07-20 22:32:45 +0800

    轮询路由规则缓存，修改为并发map

--- pig-cloud-gateway-service/service/cachePool.go
+++ pig-cloud-gateway-service/service/cachePool.go
@@ -2,30 +2,29 @@ package service
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 
 	"github.com/gaozhen1996/pig-cloud/pkg/logging"
 	"github.com/gaozhen1996/pig-cloud/pkg/setting"
 )
 
 type CachePool struct {
-	roundRuleMap map[string]RoundRule //轮询规则缓存,key:服务名
-	serviceMap   map[string][]Server  //服务实例缓存,key:服务名
+	roundRuleMap sync.Map            //轮询规则缓存,key:服务名
+	serviceMap   map[string][]Server //服务实例缓存,key:服务名
 }
 
 //缓存池实例
 var cachePool CachePool
 
 func (cp *CachePool) GetRoundRule(serviceName string) RoundRule {
-
-	//如果map中为空，则初始化
-	if len(cp.roundRuleMap) == 0 {
-		cp.roundRuleMap = make(map[string]RoundRule)
+	var res RoundRule
+	var rr, ok = cp.roundRuleMap.Load(serviceName)
+	if ok {
+		res = rr.(RoundRule)
 	}
-
-	return cp.roundRuleMap[serviceName]
+	return res
 }
 
 func (cp *CachePool) GetService(serviceName string) []Server {
@@ -35,7 +34,6 @@ func (cp *CachePool) GetService(serviceName string) []Server {
 		cp.serviceMap = make(map[string][]Server)
 	}
 	var servers = cp.serviceMap[serviceName]
-	fmt.Println()
 	if len(servers) == 0 {
 		servers = getHostFromRegister(serviceName)
 		cp.serviceMap[serviceName] = servers
