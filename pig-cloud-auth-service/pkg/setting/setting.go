e62cea7690336346c3fddc121a3e710adb746235
gaozhen <gaozhen3h@163.com>
2021-08-04 23:40:43 +0800

    docker 缩小体积完成

--- pig-cloud-auth-service/pkg/setting/setting.go
+++ pig-cloud-auth-service/pkg/setting/setting.go
@@ -23,7 +23,7 @@ var (
 
 func init() {
 	var err error
-	Cfg, err = ini.Load("conf/app.ini")
+	Cfg, err = ini.Load("conf/bootstrap.ini")
 	if err != nil {
 		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
 	}
