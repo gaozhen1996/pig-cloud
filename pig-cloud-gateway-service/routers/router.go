09414a00a59c514c31e21d8ca07d5ab89bca759a
gaozhen <3168175817@qq.com>
2021-08-28 14:18:48 +0800

    添加跨域支持

--- pig-cloud-gateway-service/routers/router.go
+++ pig-cloud-gateway-service/routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gaozhen1996/pig-cloud/pkg/setting"
 	"github.com/gaozhen1996/pig-cloud/routers/api"
@@ -24,7 +25,26 @@ func InitRouter() *gin.Engine {
 
 	api.SentinelInit()
 	api.CreateFlowRule("FlowLimit", 500, 100)
-	r.Use(api.FlowLimit)
+	r.Use(handleRequest)
 
 	return r
+}
+
+func handleRequest(c *gin.Context) {
+	method := c.Request.Method
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方
+	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	c.Header("Access-Control-Allow-Credentials", "true")
+
+	//放行所有OPTIONS方法
+	if method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
+		c.Next()
+	} else {
+		api.FlowLimit(c)
+	}
+
 }
